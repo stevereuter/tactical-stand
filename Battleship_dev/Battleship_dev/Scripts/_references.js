f4552bbde635f4a332b55bb2f1c180fd7717c2bf
Steve Reuter <spoofty@hotmail.com>
2015-02-27 17:49:14 -0500

    Created main framework.

---
+++ Battleship_dev/Battleship_dev/Scripts/_references.js
@@ -0,0 +1,2 @@
+﻿/// <reference path="C:\Repo\Battleship\Battleship_dev\Battleship_dev\js/bootstrap.js" />
+/// <reference path="C:\Repo\Battleship\Battleship_dev\Battleship_dev\js/jquery-2.1.3.js" />
